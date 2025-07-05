c7e36fd41165fd0586387c41ca21a83b5b76b6cc
JKS <javier.kuri@anahuac.mx>
2025-07-04 18:39:46 -0600

    Added functions for ordering the counter in descending order

--- compressor.py
+++ compressor.py
@@ -16,5 +16,12 @@ def word_counter(text):
             counter[curr_word] += 1
         else:
             counter[curr_word] = 1
-
     return counter
+
+def by_value(item):
+    return item[1]
+
+def sort_dict_by_values(dictionary):
+    sorted_items = sorted(dictionary.items(), key=by_value, reverse=True)
+    sorted_dict = dict(sorted_items)
+    return sorted_dict
